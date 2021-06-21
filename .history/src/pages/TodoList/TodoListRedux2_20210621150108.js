cbddab5d82ae3fdc6247bda7d952e3a2d0901274
phubierd <phubierd@gmail.com>
2021-06-21 18:00:06 +0700

    '...'

--- .history/src/pages/TodoList/TodoListRedux2_20210621150108.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TodoListRedux2() {
-    return (
-        <div>
-            12312
-        </div>
-    )
-}
--- .history/src/pages/TodoList/TodoListRedux2_20210621155217.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Input from './TodoListRedux2/Input'
-export default function TodoListRedux2() {
-    return (
-        <div>
-            <Input/>
-        </div>
-    )
-}
